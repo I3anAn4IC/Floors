c4e0cc33878b66511a4c4dea242ee4c5a4e1fa45
MrDengazen <denchiksrty@gmail.com>
2022-05-18 19:51:14 +0400

    new

---
+++ Assets/Scripts/Volumee.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volumee : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(sliderValue) * 20);
+    }
+}
