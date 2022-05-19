f8fbf149eeaf386d4206bf37c9f1c91d3f32a604
itcubecommunity <denis@itcube.community>
2022-05-19 15:29:48 +0400

    Sound is updated

--- Assets/Scripts/StaticMusicObject.cs
+++ Assets/Scripts/StaticMusicObject.cs
@@ -1,31 +1,27 @@
+using System.Linq;
 using Fungus;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class Volumee : MonoBehaviour
+public class StaticMusicObject : MonoBehaviour
 {
     public AudioMixer audioMixer;
-
     private void Awake()
     {
         var audioSources = FungusManager.Instance.GetComponents<AudioSource>();
+        var mgroup = audioMixer.FindMatchingGroups("Master").First();
         foreach (var audioSource in audioSources)
         {
-            audioSource.outputAudioMixerGroup = audioMixer.outputAudioMixerGroup;
+            audioSource.outputAudioMixerGroup = mgroup;
         }
+
         if (!FungusPrefs.HasKey(0, "Volume"))
         {
             FungusPrefs.SetFloat(0,"Volume", 1f);
             FungusPrefs.Save();
         }
-        audioMixer.SetFloat("volume", FungusPrefs.GetFloat(0,"Volume"));
+        DebugLog.print(FungusPrefs.GetFloat(0, "Volume"));
+        audioMixer.SetFloat("volume", FungusPrefs.GetFloat(0, "Volume"));
     }
 
-    public void SetVolume(float sliderValue)
-    {
-        var v = -80 + Mathf.Log10(sliderValue) * 100;
-        FungusPrefs.SetFloat(0,"Volume", v);
-        audioMixer.SetFloat("volume", v);
-        FungusPrefs.Save();
-    }
 }
